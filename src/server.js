81dc5fa264fb50fed5899884a0749eb0889dd0c9
bion0625 <bion0625@gmail.com>
2023-09-12 16:37:59 +0900

    parser limit

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import videoRouter from "./router/videoRouter";
 import { localsMiddleware } from "./middleware";
 import apiRouter from "./router/apiRouter";
 import textRouter from "./router/textRouter";
+import workRouter from "./router/workRouter";
 
 const app = express();
 const logger = morgan("dev");
@@ -18,8 +19,8 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit : "50mb" }));
+app.use(express.json({limit : "50mb"}));
 
 app.use(session({
     secret:process.env.COOKIE_SECRET,
@@ -38,5 +39,6 @@ app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 app.use("/text", textRouter);
 app.use("/api", apiRouter);
+app.use("/work", workRouter)
 
 export default app;
